23f935b29a426439bcd89ab26014f89a0e2c55ff
Breadus1 <116367179+Breadus1@users.noreply.github.com>
2023-11-16 12:34:05 +0100

    Add files via upload

---
+++ bazadanych.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS tekst (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tresc TEXT
+);
+
+INSERT INTO tekst (tresc) VALUES ('Polityka i Społeczeństwo');
+INSERT INTO tekst (tresc) VALUES ('Europa w XVII wieku charakteryzowała się wojnami religijnymi i dynastycznymi, w tym Wojną Trzydziestoletnią.');
+INSERT INTO tekst (tresc) VALUES ('Wzrost absolutyzmu monarchicznego, szczególnie we Francji pod rządami Ludwika XIV.');
+INSERT INTO tekst (tresc) VALUES ('Angielska wojna domowa i następnie restauracja monarchii w 1660 roku.');
+
+INSERT INTO tekst (tresc) VALUES ('Nauka i Oświecenie');
+INSERT INTO tekst (tresc) VALUES ('Rewolucja Naukowa z postaciami takimi jak Galileusz, Newton, i Kepler.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój Metody Naukowej, kładącej nacisk na empiryzm i eksperymentowanie.');
+
+INSERT INTO tekst (tresc) VALUES ('Kultura i Sztuka');
+INSERT INTO tekst (tresc) VALUES ('Dominacja stylu barokowego w sztuce i architekturze.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój literatury europejskiej z postaciami takimi jak William Szekspir, Miguel de Cervantes, i Molière.');
+
+INSERT INTO tekst (tresc) VALUES ('Odkrycia Geograficzne i Kolonializm');
+INSERT INTO tekst (tresc) VALUES ('Rozwój kolonializmu przez europejskie mocarstwa i intensyfikacja handlu transatlantyckiego, w tym handlu niewolnikami.');
+
+INSERT INTO tekst (tresc) VALUES ('Religia i Filozofia');
+INSERT INTO tekst (tresc) VALUES ('Reformacja i Kontrreformacja, mające długotrwały wpływ na religijny i kulturowy krajobraz Europy.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój filozofii z postaciami takimi jak Descartes i Spinoza, wprowadzających nowe idee.');
+
+INSERT INTO tekst (tresc) VALUES ('Znaczenie XVII Wieku');
+INSERT INTO tekst (tresc) VALUES ('XVII wiek jako okres przejściowy ukształtowujący nowoczesny świat poprzez innowacje w nauce, zmiany polityczne i społeczne, oraz wpływ na kulturę.');
+
+INSERT INTO tekst (tresc) VALUES ('Autor: Marcel Wątruch 2a');
